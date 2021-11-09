c1764ac749bcbdbd152661c31be4f2caac8bdce7
denisklg <dnsklg@yandex.ru>
2021-11-10 01:10:14 +0500

    6. Checkstyle [#4123]

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -4,5 +4,6 @@ public class StudentInfo {
     public static void main(String[] args) {
         System.out.println("Denis Kolganov");
         System.out.println("20.10.1994");
+        System.out.println("Miass");
     }
 }
